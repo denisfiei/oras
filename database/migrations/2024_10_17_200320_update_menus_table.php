88d38386c08995942e9e15ea1fa16b20285876a1
Rafael Juan Medina Palacios <rmedina@saludpol.gob.pe>
2024-10-17 20:08:36 -0500

    La migracion para los nuevos items del menu

---
+++ database/migrations/2024_10_17_200320_update_menus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class UpdateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('menus')->insert([
+
+            ['categoria'=>'O', 'nombre'=>'Tipo Dengue', 'icono'=>'fas fal fa-vial', 'route'=>'tipo_dengue', 'url'=>'tipo_dengue', 'orden'=>7, 'user_id'=>1],
+            ['categoria'=>'O', 'nombre'=>'Carga de Datos', 'icono'=>'fas fa-file-upload', 'route'=>'carga_dengue', 'url'=>'carga_dengue', 'orden'=>8, 'user_id'=>1],
+            ['categoria'=>'O', 'nombre'=>'Serotipo / Genotipo', 'icono'=>'fas fab fa-creative-commons-zero', 'route'=>'serotipo_genotipo', 'url'=>'serotipo_genotipo', 'orden'=>9, 'user_id'=>1],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
